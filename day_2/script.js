4ea9b093efc36d7b8b39a7c07ebf1a02a0de7db1
pankaj <88972716+pankajsahh@users.noreply.github.com>
2023-04-26 10:22:31 +0530

    Add files via upload

---
+++ day_2/script.js
@@ -0,0 +1,82 @@
+
+let x
+
+console.log(x)
+x=12;
+console.log(x)
+
+function walk(){
+
+}
+(()=>{
+
+})()
+
+walk()
+
+
+function walk(num1,num2){
+    var name = "panakj"
+return this.helth+ num1+num2;
+let name = "suraj"
+name.split(0,2)
+if(name){
+    let name2 = "somethinf"
+}
+}
+walk(1,2)
+
+Boolean.length
+
+class  Car{
+    constructor(){
+        this.engine = "120hp"
+        this.name="mercese"
+    }
+    drive(){
+        return this.engine;
+    }
+}
+
+const data = {
+    name:124,
+    child:{
+        child:123
+    }
+}
+
+let newData = {...data} /// shallow copy
+
+console.log(newData)
+
+data.child.child = 12;
+// const newData = JSON.parse(JSON.stringify(data))
+
+const data  = "dso"
+console.log(data)
+const newdata = {...data}
+// const 
+// let newcar = new Car()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(true){
+    var name = "panakj"
+    let name23 = "panakj"
+    const name3 = "panakj"
+
+}
+console.log(name)
